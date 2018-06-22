d89dd08ed5cca0842a284c51f8e08991c69f0d96
Spencer C. Gabhart <sgabhart2230@gmail.com>
2018-06-22 11:42:11 -0400

    Set up form to accept turt info

--- src/app/add-dialog/add-dialog.component.ts
+++ src/app/add-dialog/add-dialog.component.ts
@@ -16,14 +16,21 @@ import { MdcDialog,
 export class AddDialogComponent implements OnInit {
   addForm: FormGroup;
   submitted = false;
+
   @ViewChild('inputName') inputName: MdcTextField;
+  @ViewChild('inputClass') inputClass: MdcTextField;
+  @ViewChild('inputDesc') inputDesc: MdcTextArea;
+  @ViewChild('inputUrl') inputUrl: MdcTextField;
 
   constructor(public dialogRef: MdcDialogRef<AddDialogComponent>,
   			  @Inject(MDC_DIALOG_DATA) public data: any) { }
 			  
   ngOnInit() { 
     this.addForm = new FormGroup({
-	  inputName: new FormControl({ value: '', disabled: false }, Validators.required)
+		inputName: new FormControl({ value: '', disabled: false }, Validators.required),
+		inputClass: new FormControl({ value: '', disabled: false }, Validators.required),
+		inputDesc: new FormControl({ value: '', disabled: false }, Validators.required),
+		inputUrl: new FormControl({ value: '', disabled: false }, Validators.required)
 	});
   }
 
@@ -41,11 +48,27 @@ export class AddDialogComponent implements OnInit {
 	    if(!this.inputName.valid) {
 		  this.inputName.setValid(false);
 		}
+		if(!this.inputClass.valid) {
+		  this.inputClass.setValid(false);	  
+		}
+	    if(!this.inputDesc.valid) {
+		  this.inputDesc.setValid(false);  
+		}
+		if(!this.inputUrl.valid) {
+		  this.inputUrl.setValid(false);
+		}
+
 		return;
 	  }		
 			
-	  
-	
+	  var mName = this.inputName.getValue();
+	  var mClass = this.inputClass.getValue();
+	  var mDesc = this.inputDesc.getValue();
+	  var mUrl = this.inputUrl.getValue();
+
+	  var mTurtle = new Turtle(mName, mClass, mDesc, mUrl);
+	  console.log(JSON.stringify(mTurtle));
+
 	  this.submitted = true;
 	  this.closeDialog();
 	}
